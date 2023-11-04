c128f37ce3930246815684b95b37cd2f31760a89
Khym Nad <nad@cs.ualberta.ca>
2023-11-04 06:14:37 -0600

    added quit choices to search_users, select_user, and follow_user

--- main_functions.py
+++ main_functions.py
@@ -66,17 +66,21 @@ def show_previous(hidden, shown):
     select_user(hidden, shown)
 
 def select_user(hidden, shown):
-    text = (input("Input list number to view user (enter n to show next, p to show previous): ")).lower()
-    if (text == 'n'):
-        show_more(hidden, shown)
-    elif (text == 'p'):
-        show_previous(hidden, shown)
-    elif (text in ['1','2','3','4','5']): # a user is selected
-        display_current(hidden, shown, text)
-        follow_user(hidden, shown, shown[5-int(text)][0], current_userID)
-    else: # catch all invalid inputs
-        for count in range(1,5):
-            print(count, shown[5-count][0])   
+    end = False
+    while(end != True):
+        text = (input("Input list number to view user (enter n to show next, p to show previous, q to quit current search): ")).lower()
+        if (text == 'n'):
+            show_more(hidden, shown)
+        elif (text == 'p'):
+            show_previous(hidden, shown)
+        elif (text == 'q'): # when quit, return to search_users()
+            end = True
+        elif (text in ['1','2','3','4','5']): # a user is selected
+            display_current(hidden, shown, text)
+            follow_user(hidden, shown, shown[5-int(text)][0], current_userID)
+        else: # catch all invalid inputs
+            for count in range(1,6):
+                print(count, shown[5-count][0])   
 
 def display_data(user):
     # number of tweets
@@ -130,11 +134,13 @@ def display_data(user):
     print("--------------------------------------------------------")
 
 def follow_user(hidden, shown, flwee, current_userID):
-    text = (input("Enter 0 to follow (enter n to show next, p to show previous) : ")).lower()
+    text = (input("Enter 0 to follow or view other user (enter n to show next, p to show previous, q to quit current search) : ")).lower()
     if (text == 'n'):
         show_more(hidden, shown)
     elif (text == 'p'):
         show_previous(hidden, shown)
+    elif (text == 'q'): # when quit, return to search_users()
+        search_users()
     elif (text == '0'): # current_user has chosen to follow selected user
         # finds the user.usr of the selected user
         cursor.execute('''
@@ -168,7 +174,7 @@ def display_current(hidden, shown, text):
                 print("--------------------------------------------------------")
                 print("user: ", shown[5-int(text)][0])
                 display_data(shown[5-int(text)][0])
-    else:
+    else: # there are less than 5 users displayed
         for count in range(1,amount_displayed+1):
             print(count, shown[amount_displayed-count][0])
             if (count == int(text)):
@@ -179,39 +185,42 @@ def display_current(hidden, shown, text):
 def search_users():
     end = False
     while not end:
-        text = "%"+(input("Search Users: "))+"%"
-        # users name matches keyword
-        cursor.execute('''
-                        SELECT users.name 
-                        FROM users 
-                        WHERE users.name LIKE '%s' 
-                        ORDER BY length(users.name)
-                        ''' %(text))
-        rows1 = cursor.fetchall()
-            
-        # users city but not name match the keyword
-        cursor.execute('''
-                        SELECT users.name, users.city
-                        FROM users 
-                        WHERE users.name NOT LIKE '%s' 
-                        AND users.city LIKE '%s' 
-                        ORDER BY length(users.city)
-                        ''' %(text,text))
-        rows2 = cursor.fetchall()
-
-        # all rows are "hidden" since no users are being displayed (populated [1,2,3,4,5])
-        hidden = rows1 + rows2
-        # users being displayed (populated [5,4,3,2,1,5,4,3,2,1])
-        shown = []
-
-        if (len(hidden) == 0):
-            search_users()
+        text = "%"+(input("Search Users (q to quit): "))+"%"
+        if(text == "%"+'q'+"%"): # when quit return to chossing functionalities
+            end = True
         else:
-            # shows only 5 users and prompts the user to see more users or search again
-            count = 0
-            while (count != 5 and len(hidden) != 0):
-                # removes users from the hidden list, printing and placing them in shown
-                print(count +1, hidden[0][0])
-                count = count +1
-                shown.insert(0,hidden.pop(0))
-        select_user(hidden, shown)
+            # users name matches keyword
+            cursor.execute('''
+                            SELECT users.name 
+                            FROM users 
+                            WHERE users.name LIKE '%s' 
+                            ORDER BY length(users.name)
+                            ''' %(text))
+            rows1 = cursor.fetchall()
+                
+            # users city but not name match the keyword
+            cursor.execute('''
+                            SELECT users.name, users.city
+                            FROM users 
+                            WHERE users.name NOT LIKE '%s' 
+                            AND users.city LIKE '%s' 
+                            ORDER BY length(users.city)
+                            ''' %(text,text))
+            rows2 = cursor.fetchall()
+
+            # all rows are "hidden" since no users are being displayed (populated [1,2,3,4,5])
+            hidden = rows1 + rows2
+            # users being displayed (populated [5,4,3,2,1,5,4,3,2,1])
+            shown = []
+
+            if (len(hidden) == 0):
+                search_users()
+            else:
+                # shows only 5 users and prompts the user to see more users or search again
+                count = 0
+                while (count != 5 and len(hidden) != 0):
+                    # removes users from the hidden list, printing and placing them in shown
+                    print(count +1, hidden[0][0])
+                    count = count +1
+                    shown.insert(0,hidden.pop(0))
+            select_user(hidden, shown)
