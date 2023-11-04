12a0a93ee209bbd877b90d93e618386056e98c32
Khym Nad <nad@cs.ualberta.ca>
2023-11-04 03:38:44 -0600

    updated select_user, created display_data, added more test data

--- khym-main.py
+++ khym-main.py
@@ -1,6 +1,5 @@
 import sqlite3
 import time
-from main_functions import *
 
 def drop_tables():
     global connection, cursor
@@ -154,14 +153,23 @@ INSERT INTO users(usr, pwd, name, email, city, timezone) VALUES
 INSERT INTO follows(flwer, flwee, start_date) VALUES
     (29,97,'2021-01-10'),
     (97,29,'2021-09-01'),
-    (5,97,'2022-11-15');
+    (5,97,'2022-11-15'),
+    (108,111,'2021-09-01'),
+    (108,101,'2021-09-02'),
+    (108,102,'2021-09-03'),
+    (97,108,'2021-09-01'),
+    (111,108,'2021-09-01');
 '''
 
     insert_tweets = '''
 INSERT INTO tweets(tid, writer, tdate, text, replyto) VALUES
     (1,5,'2023-06-01','Looking for a good book to read. Just finished lone #survivor', null),
     (2,97,'2023-02-12','#Edmonton #Oilers had a good game last night.',null),
-    (3,5,'2023-03-01','Go oliers!',2);
+    (3,5,'2023-03-01','Go oliers!',2),
+    (4,108,'2023-04-01','Hello',2),
+    (5,108,'2023-06-01','Hello World',2),
+    (6,108,'2023-05-01','SQL',2),
+    (7,108,'2023-07-01','SQLite!',2);
 '''
 
     insert_hashtags = '''
@@ -208,6 +216,176 @@ INSERT INTO includes(lname, member) VALUES
 
     return
 
+def connect(path):
+    global connection, cursor
+    
+    connection = sqlite3.connect(path)
+    cursor = connection.cursor()
+    cursor.execute('PRAGMA foreign_keys = ON;')
+    connection.commit()
+    return
+
+def show_more(hidden, shown):
+    current = len(shown) % 5
+    count = 0
+    # there are no more pages to access
+    if (len(hidden) == 0):
+        for i in range(current):
+            print(i + 1, shown[current-(i+1)][0])
+        print("no next page")
+        select_user(hidden, shown)
+    # shows the next page of users
+    # if next page < 5 users shows # of users on that page
+    while (count != 5):
+        if (len(hidden) == 0):
+            print("no more users")
+            select_user(hidden, shown)
+        else:
+            # removes users from the hidden list, printing and placing them in shown
+            print(count +1, hidden[0][0])
+            shown.insert(0,hidden.pop(0))
+            count = count +1
+    select_user(hidden, shown)
+
+def hide(hidden, shown, amount):
+    # places the users shown back into hidden in order [1,2,3,4,5]
+    if (amount == 0): # when 5 users were displayed
+        for i in range(5):
+            hidden.insert(0,shown.pop(0)) 
+    else: # when < 5 users were displayed
+        for i in range(amount):
+            hidden.insert(0,shown.pop(0))
+        
+def show_previous(hidden, shown):
+    # current is the amount of users currently displayed
+    current = len(shown) % 5
+    count = 1
+    if (len(shown) - 5 == 0):
+        while (count <= 5):
+            print(count, shown[5-count][0])
+            count = count +1
+        print("no prev. page")
+        select_user(hidden, shown)
+    else:
+        # moves the users that were currently shown back into hidden
+        # first set of [5,4,3,2,1] is the previous page
+        hide(hidden, shown, current)
+        # prints the previous page of 5 users
+        # shown populated [5,4,3,2,1,5,4,3,2,1] print starts at index 4 and decrements to 0 (5-count)
+        while (count <= 5):
+            print(count, shown[5-count][0])
+            count = count +1
+    select_user(hidden, shown)
+
+def select_user(hidden, shown):
+    text = (input("Select user to view (enter n to show next p to show previous): ")).lower()
+    if (text == 'n'):
+        show_more(hidden, shown)
+    elif (text == 'p'):
+        show_previous(hidden, shown)
+        return
+    elif (text in ['1','2','3','4','5']): # a user is selected
+        for count in range(1,6):
+            print(count, shown[5-count][0])
+            if (count == int(text)):
+                print("--------------------------------------------------------")
+                print("user: ", shown[5-int(text)][0])
+                display_data(shown[5-int(text)][0])
+    else: # catch all invalid inputs
+        for count in range(1,5):
+            print(count, shown[5-count][0])   
+
+def display_data(user):
+    # number of tweets
+    cursor.execute('''
+                    SELECT COUNT(tweets.writer) 
+                    FROM tweets 
+                    WHERE tweets.writer = (SELECT users.usr
+                                            FROM users
+                                            WHERE users.name = '%s') 
+                    ''' %(user))
+    nbr_tweets = cursor.fetchall()
+    print("Number of tweets: ",nbr_tweets[0][0])
+
+    # number of people following the user
+    cursor.execute('''
+                    SELECT COUNT(DISTINCT follows.flwee)
+                    FROM follows
+                    WHERE follows.flwer = (SELECT users.usr
+                                            FROM users
+                                            WHERE users.name = '%s')  
+                   ''' %(user))
+    nbr_flwee = cursor.fetchall()
+    print("follows: ",nbr_flwee[0][0],"users")
+
+    # number of people the user is following
+    cursor.execute('''
+                    SELECT COUNT(DISTINCT follows.flwer)
+                    FROM follows
+                    WHERE follows.flwee = (SELECT users.usr
+                                            FROM users
+                                            WHERE users.name = '%s')
+                   ''' %(user))
+    nbr_followers = cursor.fetchall()
+    print("followers: ",nbr_followers[0][0],"users")
+
+    # users tweets from newest to oldest
+    cursor.execute('''
+                    SELECT tweets.text
+                    FROM tweets
+                    WHERE tweets.writer = (SELECT users.usr
+                                            FROM users
+                                            WHERE users.name = '%s')
+                    ORDER BY julianday('now') - julianday(tweets.tdate) DESC
+                   ''' %(user))
+    user_tweets = cursor.fetchall()
+    print("Tweets: ")
+    for i in range(3):
+        print(user_tweets[i][0])
+    
+    print("--------------------------------------------------------")
+
+
+def search_users():
+    end = False
+    while not end:
+        text = "%"+(input("Search Users: "))+"%"
+        # users name matches keyword
+        cursor.execute('''
+                        SELECT users.name 
+                        FROM users 
+                        WHERE users.name LIKE '%s' 
+                        ORDER BY length(users.name)
+                        ''' %(text))
+        rows1 = cursor.fetchall()
+            
+        # users city but not name match the keyword
+        cursor.execute('''
+                        SELECT users.name, users.city
+                        FROM users 
+                        WHERE users.name NOT LIKE '%s' 
+                        AND users.city LIKE '%s' 
+                        ORDER BY length(users.city)
+                        ''' %(text,text))
+        rows2 = cursor.fetchall()
+
+        # all rows are "hidden" since no users are being displayed (populated [1,2,3,4,5])
+        hidden = rows1 + rows2
+        # users being displayed (populated [5,4,3,2,1,5,4,3,2,1])
+        shown = []
+
+        if (len(hidden) == 0):
+            search_users()
+        else:
+            # shows only 5 users and prompts the user to see more users or search again
+            count = 0
+            while (count != 5 and len(hidden) != 0):
+                # removes users from the hidden list, printing and placing them in shown
+                print(count +1, hidden[0][0])
+                count = count +1
+                shown.insert(0,hidden.pop(0))
+            select_user(hidden, shown)
+
 def main():
     global connection, cursor
 
@@ -220,7 +398,7 @@ def main():
     insert_data()
 
     # finds users where keyword searched is mentioned in the users name and city
-    search_users(cursor)                  
+    search_users()                  
             
     connection.close()
     return
